a1ee169c94aa1f11ab32e8b9b66829895ebef199
rohithelluru6 <rohithelluru6@gmail.com>
2026-02-14 11:54:06 +0530

    Create student_marks_caluclation.py

---
+++ Basic_problem_solving/student_marks_caluclation.py
@@ -0,0 +1,14 @@
+# code
+a=int(input("enter the marks in 1st subject : "))
+b=int(input("enter the marks in 2nd subject : "))
+c=int(input("enter the marks in 3rd subject : "))
+d=int(input("enter the marks in 4th subject : "))
+e=int(input("enter the marks in 5th subject : "))
+
+
+total_marks=a+b+c+d+e
+average_marks=(a+b+c+d+e)//5
+percentage=(average_marks/500)*100
+print("total marks",total_marks)
+print("average marks",average_marks)
+print("percentage",percentage)
