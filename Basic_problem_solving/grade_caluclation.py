1c77bc11e3292940d13ae864e50d656408fb797f
rohithelluru6 <rohithelluru6@gmail.com>
2026-02-16 22:51:46 +0530

    Create grade_caluclation.py

---
+++ Basic_problem_solving/grade_caluclation.py
@@ -0,0 +1,16 @@
+# code
+marks=int(input("enter the marks: "))
+if marks >=90:
+    print("grade is A")
+elif marks>=80:
+    print("grade is B")
+elif marks>=70:
+    print("grade is C")
+elif marks>=60:
+    print("grade is D")
+elif marks>=50:
+    print("grade is C")
+elif marks>=35:
+    print("grade is E")
+else:
+    print("grade is F")
